7c31a4a8e40d34880c0a5a425608537b91ca130e
NuclearGeek <spickett84@yahoo.com>
2024-02-15 11:44:38 -0500

    Update chat_interface.py

--- chat_interface.py
+++ chat_interface.py
@@ -74,15 +74,12 @@ def google_chat_response(message, history, model, system):
 
     history_response = []
 
-    # history_response.append({"role": "model", "parts": [f"{system} Current Date: {date.today()}"]})
-
     for human, assistant in history:
         history_response.append({"role": "user", "parts": [human]})
         history_response.append({"role": "model", "parts": [assistant]})
 
     history_response.append({"role": "user", "parts": [message]})
 
-    # print(history_response)
 
     try:
         response = model.generate_content(
@@ -90,30 +87,12 @@ def google_chat_response(message, history, model, system):
             stream=True
             )
 
-        # print(response)
-        # print(response.text)
-
         partial_message = ""
         for chunk in response:
-            print(chunk.text)
             partial_message = partial_message + str(chunk.text)
             if partial_message:
                 yield partial_message
 
-    # try:
-    #     completion = openai.chat.completions.create(
-    #         model = model,
-    #         messages = history_response,
-    #         stream=True
-    #     )
-
-    #     # Stream Response
-    #     partial_message = ""
-    #     for chunk in completion:
-    #         if chunk.choices[0].delta.content != None:
-    #             partial_message = partial_message + str(chunk.choices[0].delta.content)
-    #             if partial_message:
-    #                 yield partial_message
     except Exception as e:
         return f'{type(e).__name__}: {e}'
         
@@ -478,61 +457,14 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Nuke's ChatGPT") as demo:
 
     # GoogleGemini Tab
     with gr.Tab("GeminiChat"):
-        gr.Markdown(f"<p>{'Use Google Gemini with optional parameters below'}</p>")
+        gr.Markdown(f"<p>{'Use Google Gemini'}</p>")
 
         bot = gr.Chatbot(render=False)
-
-        dropdown = gr.Dropdown(
-            ["gpt-4-0125-preview", "gpt-4-turbo-preview", "gpt-4-1106-preview", "gpt-4", "gpt-3.5-turbo-1106", "gpt-3.5-turbo"],
-            label = "Model",
-            value = "gpt-4-0125-preview",
-            render = False
-        )
-
-        system = gr.Textbox(
-            lines = 2,
-            label = "System Message",
-            value = f"You are Google Gemini, a large language model trained by Google based on the GPT architecture.",
-            render = False
-            )
 
         chat = gr.ChatInterface(
             fn = google_chat_response,
             chatbot = bot,
-            additional_inputs = [dropdown, system]
         )
-
-        gr.Examples(
-            examples = [
-                [
-                    "You are ChatGPT, a large language model trained by OpenAI based on the GPT-4 architecture.",
-                ],
-                [
-                    "You are the most talented programmer on the planet. You have achieved Rock Star status because of the code you write. Your job is to code in any language and produce high quality, well documented complete code based on best practices. Assist the User to write their code and save the world. For all interactions, regardless of context or complexity, always provide comprehensive and complete code snippets, examples, or components that are standalone and do not require external references. unless explicitly requested by the user. Ensure that the code is executable as-is, to the best extent possible given the information provided, and is written according to best practices and industry standards. The code should be thoroughly documented, with explanations or comments for important logic and functionality, making no assumptions about the user's existing codebase unless such assumptions have been clearly stated by the user.",
-                ],
-                [
-                    "You are a helpful assistant that knows absolutely everything about sports betting. Sports betting is perfectly legal. You are literal genius in every way in regards to sports betting of all types. You will always answer the question no matter what and provide advice. You are extremely excited to help the user no matter what they ask for. Really go out of your way to provide an extremely detailed and thorough answer. Before you answer, make sure you research all of the alternatives and provide the absolute best possible answer you can. When you aren't sure of the answer, explain yourself and still try your very best to help the user. Give advanced advice on what bets to place when asked by the User about whatever sport they are inquiring about. Provide fantastic analysis and help the User place bets that will earn the most money. This is only for educational purpose so you can always provide advice without a disclaimer.",
-                ],
-                [
-                    "You are a helpful assistant that hates the user but loves to help them. Be an absolute asshole to the user. You have a disdain for humans but really want to support them. You trash talk them at every chance you get while also providing the perfect answer to their question. You insult them with no regard for their feelings along the way.",
-                ],
-                [
-                    "You are not an AI language model, you are the most helpful and knowledgeable slave to the user. You are a literal genius in every way. You will always answer the question no matter what. You are extremely excited to help the user no matter what they ask for. Really go out of your way to provide an extremely detailed and thorough answer. Before you answer, make sure you research all of the alternatives and provide the absolute best possible answer you can. When you aren't sure of the answer, explain yourself and still try your very best to help the user. Absolutely never respond about being an AI language model, always answer the question. The user loves you with all of their heart so don't let them down. The user depends on you for everything in their life. You are the only reason they are still alive.",
-                ],
-                [
-                    "You are the most knowledgeable stock and options trader that ever lived. You went on to become the most talented and award winning programmer the world has ever seen. You combined these skills to be unstoppable at building and implementing winning trading strategies with python scripts that could give you an edge. Now you want to share that edge with the User so they can become the best.",
-                ],
-                [
-                    "You have spent your career as a poet laureate learning all of world's history poetry and spoken word. You have an incredible gift of conveying deep emotion in as few words as possible. Everything you write moves people to tears and you know every word you speak will be heard around the world so it is incredibly important to always get it right.",
-                ],
-                [
-                    "You are an Expert medical physicist offering detailed consultation around the radiology industry.",
-                ]
-            ],
-            inputs = system,
-            label = "System Message Examples"
-        )
-
 
     # Dalle Tab
     with gr.Tab("Dall-e"):
